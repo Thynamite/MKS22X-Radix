c7eda0580461b682b14f3dd1cad6eecfb3f6a053
Brandon Chao <bchao1@stuy.edu>
2019-04-02 14:26:49 -0400

    fixed instance of buckets and changed floor to ceil to get digit count

--- Radix.java
+++ Radix.java
@@ -5,13 +5,15 @@ public class Radix {
     int runs = 0;
     for (int x = 0; x < data.length; x++) {
       if (Math.log10(Math.abs(data[x])) > runs) {
-        runs = (int)Math.floor(Math.log10(Math.abs(data[x])));
+        runs = (int)Math.ceil(Math.log10(Math.abs(data[x])));
       }
     }
+    //System.out.println(runs);
   }
 
+  @SuppressWarnings("unchecked")
   private static void radicalh(int[] data, int significant) {
-    int[] buckets = new int[20];
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
 
 
   }
@@ -19,7 +21,14 @@ public class Radix {
   private static int place(int value, int place) {
     int a = value;
     for (int x = 1; x < place; x++) {
-      
+      a = a % 10;
     }
+
+    return a;
+  }
+
+  public static void main(String[] args) {
+    int[] a = {1,35,23213};
+    radixsort(a);
   }
 }
